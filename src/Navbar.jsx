149734aa4fc3e4f75ce6ff9e242f26140461c6de
Pawan Sharma <pawan2.radhe@gmail.com>
2024-02-08 14:33:02 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                     <div className='col-10 mx-auto'>
                         <nav className="navbar navbar-expand-lg bg-light">
                             <div className="container-fluid">
-                                <NavLink className="navbar-brand" to="/" style={{fontFamily: 'math', borderBottom: 'none'}}>The Pawan Sharma</NavLink>
+                                <NavLink className="navbar-brand" to="/" style={{fontFamily: 'math', borderBottom: 'none'}}>The Pawan Kumar Sharma</NavLink>
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
